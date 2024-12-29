164adba9db2cc6cf1cbe81b15152fa4577008237
capedcrusadrr <itsvigneshvi@gmail.com>
2024-12-29 14:27:07 +0530

    first commit of todo-list

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,28 @@
+export default function TodoInput(props) {
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter a todo..."
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleAddTodos(todoValue);
+            setTodoValue("");
+          }
+        }}
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        ADD
+      </button>
+    </header>
+  );
+}
